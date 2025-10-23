f09be52b00efd4454b4a3773a3e452dcc1bfd60f
jdtoscano94 <juan_toscano@brown.edu>
2025-10-23 18:13:09 -0400

    Operator Learning

---
+++ Operator_Learning/bubble_don/vrba_sample/vrba_sample.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -J vrba_sample
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=80:00:00
+#SBATCH --mem=64GB
+#SBATCH --partition=3090-gcondo
+#SBATCH --gres=gpu:1
+#SBATCH -o /users/jdtoscan/data/jdtoscan/RBA+Operators/bubble_don/Output/vrba_sample-%j.out
+#SBATCH -e /users/jdtoscan/data/jdtoscan/RBA+Operators/bubble_don/Error/vrba_sample-%j.err
+
+cd /users/jdtoscan/data/jdtoscan/RBA+Operators/bubble_don/vrba_sample/|| exit
+
+nvidia-smi
+source /gpfs/runtime/opt/anaconda/2020.02/etc/profile.d/conda.sh
+conda activate Pytorch_conda
+
+rm -rf ./__pycache__/
+
+python3 -u train_don.py 
